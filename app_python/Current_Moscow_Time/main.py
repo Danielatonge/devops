87e29de88fbeb86072069405c3b602aee48bb129
elamboatonge <elamboatonge@gmail.com>
2021-09-27 23:54:50 +0300

    persistence in app

--- app_python/Current_Moscow_Time/main.py
+++ app_python/Current_Moscow_Time/main.py
@@ -2,10 +2,11 @@
 """
 
 from datetime import datetime
+from os import path
+
 import requests
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-
+from fastapi.responses import HTMLResponse, StreamingResponse
 
 app = FastAPI()
 
@@ -35,7 +36,25 @@ async def read_root():
     if res.status_code == 200:
         full_date = datetime.strptime(res.json()["datetime"], "%Y-%m-%dT%H:%M:%S.%f%z")
         current_time = f"{full_date.hour}:{full_date.minute}:{full_date.second}"
+        await record_time(current_time)
         return HTMLResponse(content=html_content % current_time, status_code=200)
 
     error = f"Error: {res.status_code}"
     return HTMLResponse(content=html_content % error, status_code=404)
+
+
+async def record_time(time):
+    with open("visit.txt", "a") as output:
+        output.write(f"Recorded-time: {time}\n")
+
+
+@app.get("/visits")
+async def load_visiting():
+    """ The function response to the "/visits" endpoint by sending some response
+        with current moscow time which "/" was accessed
+    """
+    if path.exists('visit.txt'):
+        with open('visit.txt', 'r') as output:
+            return output.readlines()
+
+    return "Visit '/' route"
